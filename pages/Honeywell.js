75ab92d24fea766fb7e867a282414a7101357adc
sergiohernandezg <ser9io.hernandez@gmail.com>
2025-11-16 01:20:43 -0600

    update Form

--- pages/Honeywell.js
+++ pages/Honeywell.js
@@ -2,17 +2,52 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const form = document.querySelector('#Form');
 
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('click', function(event) {
         event.preventDefault(); 
 
-        Swal.fire({
-            title: "Thanks for your answer!",
-            text: "Your data has been submitted successfully.",
-            icon: "success"
-        }).then((result) => {
+            Swal.fire({
+            title: "Personal Data",
+            html: `
+                <form id="Form">
+                    <label for="name">Name:</label><br>
+                    <input type="text" id="name" name="name"><br><br>
+
+                    <label for="LastName">Last Name:</label><br>
+                    <input type="text" id="LastName" name="LastName"><br><br>
+
+                    <label for="email">email:</label><br>
+                    <input type="email" id="email" name="email"><br><br>
+
+                    <label for="phone">Phone:</label><br>
+                    <input type="tel" id="phone" name="phone"><br><br>
+                </form>
+            `,
+            showCancelButton: true,
+            confirmButtonText: "Submit",
+            showLoaderOnConfirm: true,
+            preConfirm: async (login) => {
+                try {
+                // Back to home page
+                window.location.href = "Honeywell.html"; 
+
+                } catch (error) {
+                Swal.showValidationMessage(`
+                    Request failed: ${error}
+                `);
+                }
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+            }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                title: `${result.value.login}'s avatar`,
+                imageUrl: result.value.avatar_url
+                });
+            }
+            }).then((result) => {
             if (result.isConfirmed) {
                 // Back to home page
-                window.location.href = "../Honeywell.html"; 
+                window.location.href = "Honeywell.html"; 
             }
         });
     });
